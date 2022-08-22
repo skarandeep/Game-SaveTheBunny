8ca6f41f217c8dc879056be8c817399d2544fb35
skarandeep <108695031+skarandeep@users.noreply.github.com>
2022-08-22 14:47:19 -0400

    Explosion class created

--- app/src/main/java/com/example/game_savethebunny/Explosion.java
+++ app/src/main/java/com/example/game_savethebunny/Explosion.java
@@ -1,2 +1,23 @@
-package com.example.game_savethebunny;public class Explosion {
+package com.example.game_savethebunny;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Explosion {
+        Bitmap explosion[] = new Bitmap[4];
+        int explosionFrame = 0;
+        int explosionX, explosionY;
+
+        //defining the constructor method for Explosion class
+        public Explosion(Context context){
+            explosion[0] = BitmapFactory.decodeResource(context.getResources(), R.drawable.explode0);
+            explosion[1] = BitmapFactory.decodeResource(context.getResources(), R.drawable.explode1);
+            explosion[2] = BitmapFactory.decodeResource(context.getResources(), R.drawable.explode2);
+            explosion[3] = BitmapFactory.decodeResource(context.getResources(), R.drawable.explode3);
+        }
+
+        public Bitmap getExplosion(int explosionFrame){
+          return explosion[explosionFrame];
+        }
 }
